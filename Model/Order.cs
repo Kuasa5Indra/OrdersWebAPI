55751b8b0c51b3d19fd610ff8042b91bc27d84cd
aindrap <aditya.indrapramana@kemsos.go.id>
2023-11-15 16:20:58 +0700

    Add project files.

---
+++ Model/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace OrdersWebAPI.Model
+{
+    public class Order
+    {
+        public Guid OrderId { get; set; }
+        public string OrderNumber { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int TotalAmount { get; set; }
+    }
+}
---
+++ Model/OrderItem.cs
@@ -0,0 +1,12 @@
+﻿namespace OrdersWebAPI.Model
+{
+    public class OrderItem
+    {
+        public Guid OrderItemId { get; set; }
+        public Guid OrderId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public int UnitPrice { get; set; }
+        public int TotalPrice { get; set; }
+    }
+}
